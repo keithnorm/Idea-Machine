fab88936746ad596c86918f4f0cb38b90e7ff341
Keith Norman <keithnorm@gmail.com>
2010-06-30 12:00:20 -0500

    mailer host configs

--- config/application.rb
+++ config/application.rb
@@ -46,7 +46,7 @@ module IdeaMachine
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
     
-    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+    config.action_mailer.default_url_options = { :host => 'ideamachiner.heroku.com' }
     config.action_mailer.delivery_method = :smtp
 
     config.action_mailer.smtp_settings = {
